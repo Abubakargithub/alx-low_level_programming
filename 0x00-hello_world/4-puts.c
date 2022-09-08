7f93ddb0b179df24380fd5adf3469edcc699651d
Abubakargithub <galaxyictcontact@gmail.com>
2022-09-08 07:03:50 -0700

    last

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,10 @@
 #include <stdio.h>
 
-/*
+/**
  *
- * main - Entry point
+ *  * main - prints the phrase "Programming is like building a multilingual puzzle
  *
- *  * 
- *
- *   * Description: 'the program's description'
- *
- *   * 
- *
- *   * Return: Always 0 (Success)
+ *   * Return: 0 if exited properly, non-zero otherwise
  *
  */
 
@@ -21,4 +15,5 @@ int main(void)
 		puts("\"Programming is like building a multilingual puzzle");
 
 			return (0);
+
 }
