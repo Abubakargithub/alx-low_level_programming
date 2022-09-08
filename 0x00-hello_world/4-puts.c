848038fd3ea5c2943339983aaba48537cb8ae139
Abubakargithub <galaxyictcontact@gmail.com>
2022-09-08 12:16:25 -0700

    tested

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,19 +1,10 @@
 #include <stdio.h>
-
-
-
 /**
- * main - prints the phrase "Programming is like building a multilingual puzzle
- *
- * Return: 0 if exited properly, non-zero otherwise
- *
+ * main - main block
+ * Return: 0
  */
-
 int main(void)
-  
 {
-  
-       puts("\"Programming is like building a multilingual puzzle");
-       return (0);
-  
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
