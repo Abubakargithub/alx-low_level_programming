c56d8b4365d0976ddf29e3c5e2352b7154ed773f
Abubakargithub <galaxyictcontact@gmail.com>
2022-09-08 08:38:12 -0700

    edited again

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * return: 0 from printing the function given
+ * */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
