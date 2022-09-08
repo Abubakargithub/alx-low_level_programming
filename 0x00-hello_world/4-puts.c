59bb8e322fa2e6c99e1fee40765b3f00bc8373d6
Abubakargithub <galaxyictcontact@gmail.com>
2022-09-08 07:19:59 -0700

    commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
+
 /**
  *
- *  * main - prints the phrase "Programming is like building a multilingual puzzle
+ * main - prints the phrase "Programming is like building a multilingual puzzle
  *
- *   * Return: 0 if exited properly, non-zero otherwise
+ * Return: 0 if exited properly, non-zero otherwise
  *
  */
 
