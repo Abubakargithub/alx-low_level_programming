1830278860a7d91c2fdf60ad169808ea355e8a97
Abubakargithub <galaxyictcontact@gmail.com>
2022-09-09 15:36:57 -0700

    3 alphabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - block
+ * Return: 0
+ */
+int main(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z' ; a++)
+		putchar(a);
+	for (a = 'A'; a <= 'Z'; a++)
+		putchar(a);
+	putchar('\n');
+	return (0);
+}
