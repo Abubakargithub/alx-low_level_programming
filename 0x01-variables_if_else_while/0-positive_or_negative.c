00ecf893fdbff18be62c5a42b9f30f44dd14d289
Abubakargithub <galaxyictcontact@gmail.com>
2022-09-09 07:13:46 -0700

    edyed work

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,10 +14,10 @@ int main(void)
 	if (n < 0)
 	{
 		printf("%d is negative\n", n);
-	}else if (n == 0)
+	} else if (n == 0)
 	{
-		printf("%d zero\n", n);
-	}else
+		printf("%d is zero\n", n);
+	} else
 	{  
 		printf("%d is positive\n", n);
 	}
