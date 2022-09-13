b5801667836733dbe5212d75ee154cf71bfb9ede
Abubakargithub <galaxyictcontact@gmail.com>
2022-09-13 07:22:40 -0700

    is loer case

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,11 @@
+#include "maim.h"
+/**
+ * _islower - this is a function to check wether the char is lower case
+ * Return: 0
+ */
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+		return (1);
+	return (0);
+}
