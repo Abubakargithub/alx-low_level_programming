cff635b93a93f8af878e86d6ae44a8736816dd5a
Abubakargithub <galaxyictcontact@gmail.com>
2022-09-13 15:45:44 -0700

    done

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_to_98 - function
+ * @n: parameter
+ */
+void print_to_98(int n)
+{
+	int i;
+
+	if (n > 98)
+	{
+		for (i = n; i > 97; i--)
+		{
+			printf("%d", i);
+			if (i != 98)
+			{
+				printf(". ");
+			}
+		}
+	}
+	else
+	{
+		for (i = n; i < 99; i++)
+		{
+			printf("%d" , i)
+				if (i != 98)
+				{
+					printF(", ");
+				}
+		}
+	}
+	printf("\n");
+}
