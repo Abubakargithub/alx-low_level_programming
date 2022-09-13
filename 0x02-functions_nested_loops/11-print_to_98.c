5653e054dd1e4901907815f3a5a568db587d58b2
Abubakargithub <galaxyictcontact@gmail.com>
2022-09-13 16:00:04 -0700

    done

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -15,7 +15,7 @@ void print_to_98(int n)
 			printf("%d", i);
 			if (i != 98)
 			{
-				printf(". ");
+				printf(".");
 			}
 		}
 	}
@@ -26,7 +26,7 @@ void print_to_98(int n)
 			printf("%d", i);
 				if (i != 98)
 				{
-					printf(", ");
+					printf(",");
 				}
 		}
 	}
