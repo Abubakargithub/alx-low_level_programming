061ee3e3cfca5eb6b2e7abea9f4fb2c51a3ff7a5
Abubakargithub <galaxyictcontact@gmail.com>
2022-09-14 12:14:39 -0700

    done

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,15 +1,14 @@
 #include "main.h"
-
 /**
  * main - block
  * Return: 0
  */
 int main(void)
 {
-int i;
+	int i;
 
-i = 0;
-positive_or_negative(i);
+	i = 98 % 2;
+	positive_or_negative(i);
 
- return (0);
+	return (0);
 }
