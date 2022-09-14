ea45d877f049d7011bab341cff5cecc9797e4e43
Abubakargithub <galaxyictcontact@gmail.com>
2022-09-13 17:09:56 -0700

    done

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+/**
+ * main - block
+ * Return: 0
+ */
+int main(void)
+{
+	int c = 0;
+	int sum = 0;
+
+	while (c < 1024)
+	{
+		if (c % 3 == 0 || c % 5 == 0)
+		{
+			sum += c;
+		}
+		c++;
+	}
+	printf("%i\n", sum);
+	return (0);
+}
