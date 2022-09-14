0cc04e0887712596970e7e671a001eec952855ca
Abubakargithub <galaxyictcontact@gmail.com>
2022-09-14 12:26:28 -0700

    done

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - cause an infinite
+ * Return: 0
+ */
+int main(void)
+{
+	/*
+	 * int i;
+	 */
+	printf("Infinite loop incoming :(\n");
+	/*
+	 * i = 0;
+	 */
+	printf("Infinte loop avoided! \\o/\n");
+
+	return (0);
+}
