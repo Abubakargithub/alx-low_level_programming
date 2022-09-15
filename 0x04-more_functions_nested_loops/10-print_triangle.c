ecc9f4c43fde68ada60f2ac46714c2ba7c624fef
Abubakargithub <galaxyictcontact@gmail.com>
2022-09-15 16:21:22 -0700

    done

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * print_triangle - function
+ * @size: parameter
+ * Return: success
+ */
+void print_triangle(int size)
+{
+	int a, b;
+
+	if (size > 0)
+	{
+		for (a = 0; a < size ; a++)
+		{
+			for (b = 0; b < size ; b++)
+			{
+				if (b < size - (a + 1))
+				{
+					_putchar(' ');
+				} else
+				{
+					_putchar(35);
+				}
+			}
+			_putchar('\n');
+		}
+	} else
+	{
+		_putchar('\n');
+	}
+}
