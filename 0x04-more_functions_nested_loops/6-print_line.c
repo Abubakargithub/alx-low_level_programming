8f75e5858ae54825d27c767f3a108d0967b6b9b2
Abubakargithub <galaxyictcontact@gmail.com>
2022-09-15 15:36:33 -0700

    done

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_line - function
+ * @n: parameter
+ */
+void print_line(int n)
+{
+	if (n <== 0)
+	{
+		_putchar('\n');
+	} else
+	{
+		int i;
+
+		for (i = 1; i <= n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+}
