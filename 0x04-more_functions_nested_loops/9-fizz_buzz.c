3698cb90baa4d7b524be86aad94dc39a55e5df6e
Abubakargithub <galaxyictcontact@gmail.com>
2022-09-15 16:38:17 -0700

    done

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,30 @@
+#include "<stdio.h>
+/**
+ * main- block
+ * Return: 0
+ */
+
+int main(void)
+{
+	int m;
+
+	for (m = 1 ; m < 100 ; m++)
+	{
+		if ((m % 3) == 0 && (m % 5) == 0)
+		{
+			printf("FizzBuzz ");
+		} else if (m % 3 == 0)
+		{
+			printf("Fizz ");
+		} else if (m % 5 == 0)
+		{
+			printf("Buzz ");
+		} else
+		{
+			printf("%d ", m);
+		}
+	}
+	printf("Buzz");
+	printf("\n");
+	return (0);
+}
