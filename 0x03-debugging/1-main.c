6df2b4f42fd95d286aa91e7a6fddbc8059addf87
Abubakargithub <galaxyictcontact@gmail.com>
2022-09-15 02:54:09 -0700

    done

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - block
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	printf("Infinite loop incoming :(\n");
+
+	i = 0;
+
+	printf("Infinite loop avoided! \\o/\n");
+
+	return (0);
+}
