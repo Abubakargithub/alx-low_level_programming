5eae236fe808608e3b20ea3e43871222bca8dca5
Abubakargithub <galaxyictcontact@gmail.com>
2022-09-15 16:49:35 -0700

    done

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_square - function
+ * @size: parameter
+ * Return: 0
+ */
+void print_square(int size)
+{
+	int a, b;
+
+	if (size > 0)
+	{
+		for (a = 0; a < size ; a++)
+		{
+			for (b = 0; b < size ; b++)
+			{
+				_putchar(35);
+			}
+			_putchar('\n');
+		}
+	} else
+	{
+		_putchar('\n');
+	}
+}
+
