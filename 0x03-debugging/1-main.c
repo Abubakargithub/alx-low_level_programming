c40352b0f9284747b059d4de78dc74e41a08a4de
Abubakargithub <galaxyictcontact@gmail.com>
2022-09-15 01:35:46 -0700

    betty

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -5,21 +5,13 @@
  */
 int main(void)
 {
-	
-	 int i;
-	 
+	int i;
+
 	printf("Infinite loop incoming :(\n");
 
 	i = 0;
 
-	while (i < 10)
-	{
-		putchar(i);
-	}
-	/*
-	 * i = 0;
-	 */
-	printf("Infinte loop avoided! \\o/\n");
+	printf("Infinite loop avoided! \\o/\n");
 
-	return (0);
+	return (0)
 }
