cebba103285886b5b51ce3cb299197e09f3fddba
Abubakargithub <galaxyictcontact@gmail.com>
2022-09-15 12:44:39 -0700

    done

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _isupper - checks if letters are upper
+ * @c: input
+ * Return: 1 
+ */
+int _isupper(int c)
+{
+	if ((c >= 'A'A) && (c <= 'Z'))
+	{
+		return (1);
+	} else
+	{
+		return (0);
+	}
+}
