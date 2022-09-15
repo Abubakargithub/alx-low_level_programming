869cd4c13d01188333adf64c4eefe1f173b56c81
Abubakargithub <galaxyictcontact@gmail.com>
2022-09-15 14:50:38 -0700

    done

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,13 +1,19 @@
 #include "main.h"
 /**
- * _isdigit - function
- * @c: parameter 
- * Return: 1
- */
+*_isdigit - function
+*@c: parameter
+*Return: 1
+*/
+
 int _isdigit(int c)
 {
-	if (c >= 48 && c <= 57)
-		return (1);
-	else
-		return (0);
+is (c >= 48 && c <= 57)
+{
+return (1);
+}
+
+else
+{
+return (0);
+}
 }
