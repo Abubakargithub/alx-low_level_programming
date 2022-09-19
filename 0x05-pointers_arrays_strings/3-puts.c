2c07e0479945c01a031d3fcb134082263a7cc75a
Abubakargithub <galaxyictcontact@gmail.com>
2022-09-19 07:38:14 -0700

    puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _puts - prints string 
+ * @str: pointer to string
+ * Return: void 
+ */
+void _puts(char *str)
+{
+	int i;
+
+	i = 0;
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		i++;
+		str++;
+	}
+	_putchar('\n');
+}
