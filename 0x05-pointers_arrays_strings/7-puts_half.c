3389c99a9acdc59b98360a8f304055495ae80c0b
Abubakargithub <galaxyictcontact@gmail.com>
2022-09-19 08:47:43 -0700

    pointer

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * puts_half - print half 
+ * @str: parameter
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	int n, length, mid;
+
+	length = 0;
+	while (str[length] != '\0')
+	{
+		length++;
+	}
+
+	if (length % 2 == 0)
+	{
+		mid = length / 2;
+		for (n = mid; n < length; n++)
+		{
+			_putchar(str[n]);
+		} else
+		{
+			mid = (length - 1) / 2;
+			for (n = mid + 1; n < length; n++)
+			{
+				_putchar(str[n]);
+			}
+		}
+		_putchar('\n');
+	}
+}
+
