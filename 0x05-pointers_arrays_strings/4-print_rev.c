e65e4ea34167f47b0cc49ef6048edd179150e264
Abubakargithub <galaxyictcontact@gmail.com>
2022-09-19 08:09:48 -0700

    print

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_rev - prints strin
+ * @s: parameter
+ * Return: void 
+ */
+void print_rev(char *s)
+{
+	int length, last;
+
+	length = 0;
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+
+	last = length - 1;
+	for (; last >= 0; last--)
+	{
+		_putchar(s[last]);
+	}
+	_putchar('\n');
+}
