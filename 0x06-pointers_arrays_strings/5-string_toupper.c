8366eb7ca53b15d1f81bb13a85bdc700a5784975
Abubakargithub <galaxyictcontact@gmail.com>
2022-09-21 11:47:17 -0700

    tested

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * string_toupper - uppers a lower
+ * @s: character
+ * Return: value
+ */
+char *string_toupper(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= 97 && s[i] <= 122)
+		{
+			s[i] -= 32;
+		}
+	}
+	return (s);
+}
