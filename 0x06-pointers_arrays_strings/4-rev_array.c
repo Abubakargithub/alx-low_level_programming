4a16d18ed8d8695c99b6ec85c2c777319f121530
Abubakargithub <galaxyictcontact@gmail.com>
2022-09-21 11:53:59 -0700

    done

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * reverse_array - reverse
+ * @a: array
+ * @n: size of array
+ */
+void reverse_array(int *a, int n)
+{
+	int i;
+	int r;
+
+	for (i = 0; i < n / 2; i++)
+	{
+		r = a[i];
+		a[i] = a[n - i - 1];
+		a[n - i - 1] = r ;
+	}
+}
