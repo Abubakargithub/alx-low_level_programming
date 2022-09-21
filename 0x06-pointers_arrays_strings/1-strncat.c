c4587832908019dcf2198063f1c9f74517765c8f
Abubakargithub <galaxyictcontact@gmail.com>
2022-09-21 10:52:46 -0700

    tested

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * *_strncat - pointer function
+ * _strncat - pointer function
  * @dest: parameter
  * @src: parameter
  * @n: number
@@ -10,7 +10,7 @@ char *_strncat(char *dest, char *src, int n)
 {
 	int dest_len, i;
 
-	for (dest_len = 0; dest[dest_len] != '\0'; dest_len++);
+	for (dest_len = 0; dest[dest_len] != '\0'; dest_len++)
 
 	for (i = 0; i < n && src[i] != '\0'; i++)
 		dest[dest_len + i] = src[i];
