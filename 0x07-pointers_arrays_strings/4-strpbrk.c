70c5e980142a7567b40e2142799dfee2df56abd4
Abubakargithub <galaxyictcontact@gmail.com>
2022-09-26 08:02:38 -0700

    null

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strpbrk - bytes
+ * @s: pointer
+ * @accept: pointer
+ * Return: null
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i;
+
+	while (*s)
+	{
+		for (i = 0; accept[i]; i++)
+		{
+			if (*s == accept[i])
+			{
+				return (s);
+			}
+		}
+		s++;
+	}
+	return (NULL);
+}
