8bbbd50d389ef665ba96f0d68602b3d969b10113
Abubakargithub <galaxyictcontact@gmail.com>
2022-09-28 02:28:35 -0700

    done

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -17,6 +17,11 @@ int _sqrt_recur(int n, int x)
 	}
 	return (_sqrt_recur(n, x + 1));
 }
+/**
+ * _sqrt_recursion - function
+ * @n: num
+ * Return: sqr
+ */
 int _sqrt_recursion(int n)
 {
 	return (_sqrt_recur(b, 0));
