269b9232f6e7a1d773848c5856ee66cd99d1cb98
Abubakargithub <galaxyictcontact@gmail.com>
2022-09-28 03:43:59 -0700

    done

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -9,11 +9,10 @@ int sqrt2(int a, int b)
 {
 	if (b * b == a)
 		return (b);
-	else if (b* b > a)
+	else if (b * b > a)
 		return (-1);
 	return (sqrt2(a, b + 1));
 }
-
 /**
  * _sqrt_recursion - function
  * @n: parameter
