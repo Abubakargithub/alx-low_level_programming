cd67381f5d16cbbf005fcba899b1081a4b6de501
Abubakargithub <galaxyictcontact@gmail.com>
2022-09-28 02:24:35 -0700

    sqrt

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _sqrt_recur - helper
+ * @n: parameter
+ * @x: parameter
+ * Return: output
+ */
+int _sqrt_recur(int n, int x)
+{
+	if (x * x == n)
+	{
+		return (x);
+	}
+	else if (x * x > n)
+	{
+		return (-1);
+	}
+	return (_sqrt_recur(n, x + 1));
+}
+int _sqrt_recursion(int n)
+{
+	return (_sqrt_recur(b, 0));
+}
