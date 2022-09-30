c6494b86526f45980a79cf9797ff4817e37fdfba
Abubakargithub <galaxyictcontact@gmail.com>
2022-09-30 01:32:20 -0700

    argc

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * @argc: argc
+ * @argv: argv
+ * Return: success always
+ */
+int main(int __attribute__((__unused__)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
