4b1551dddcebbeacdf32902465c51f895fe8aaae
Abubakargithub <galaxyictcontact@gmail.com>
2022-09-30 02:16:13 -0700

    argc array

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - prints the number of arg
+ * @argc: parameter
+ * @argv: string parameter
+ * Return: 0
+ */
+int main(int argc, char __attribute__((__unused__)) *argv[])
+{
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
