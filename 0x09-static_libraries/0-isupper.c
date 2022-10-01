99b8a9879a7cf8992a556e188ca4e33a3de42f80
Abubakargithub <galaxyictcontact@gmail.com>
2022-09-30 18:02:50 -0700

    done

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _isupper - function
+ * @c: parameter
+ * Return: 1
+ */
+int _isupper(int c)
+{
+	char uppercase = 'A';
+	int isupper = 0;
+
+	for (; uppercase <= 'Z'; uppercase++)
+	{
+		if (c == uppercase)
+		{
+			isupper = 1;
+			break;
+		}
+	}
+
+	return (isupper);
+}
---
+++ 0x09-static_libraries/1-isdigit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+*_isdigit - function
+*@c: parameter
+*Return: 1
+*/
+
+int _isdigit(int c)
+{
+	return (c >= '0' && c <= '9');
+}
